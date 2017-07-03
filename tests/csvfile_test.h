93ae8f5a3aeed3cd95e679b502cd8715b6f93d68
Enzo Chimienti <e.chimienti@activevideo.com>
2017-07-03 10:39:54 +0200

    rename tests

--- tests/csvfile_test.h
+++ tests/csvfile_test.h
@@ -102,7 +102,7 @@ void clean_test_files() {
 }
 
 // string tests
-TEST(csv_write_read_file_stringTest, csv_write_readPos) {
+TEST(csv_write_read_file_string_test, csv_write_read_pos) {
 
     clean_test_files();
 
@@ -181,7 +181,7 @@ TEST(csv_two_consequtive_elements_with_quotes_test, csv_write_read_pos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_stringTest, csv_search_Pos) {
+TEST(csv_search_file_string_test, csv_search_pos) {
     clean_test_files();
 
     CsvFile<string> csv("test.csv", an_address, 8);
@@ -194,7 +194,7 @@ TEST(csv_search_file_stringTest, csv_search_Pos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_stringTest, csv_search_Neg) {
+TEST(csv_search_file_string_test, csv_search_neg) {
     clean_test_files();
 
     CsvFile<string> csv("test.csv", an_address, 8);
@@ -207,10 +207,10 @@ TEST(csv_search_file_stringTest, csv_search_Neg) {
     clean_test_files();
 }
 
-TEST(csv_search_file_quotedStringTest, csv_search_Pos) {
+TEST(csv_search_file_element_quotes_with_comma_test, csv_search_Pos) {
     clean_test_files();
 
-    an_address.push_back(an_entry_var);
+    an_address.push_back(element_with_comma_entry);
 
     CsvFile<string> csv("test.csv", an_address, 8);
     csv.write_file("in");
@@ -278,7 +278,7 @@ TEST(csv_write_read_file_int_test, csv_write_read_pos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_intTest, csv_search_Pos) {
+TEST(csv_search_file_int_test, csv_search_pos) {
     clean_test_files();
 
     CsvFile<int> csv("test.csv", a_num_vec, 5);
@@ -291,7 +291,7 @@ TEST(csv_search_file_intTest, csv_search_Pos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_intTest, csv_search_Neg) {
+TEST(csv_search_file_int_test, csv_search_neg) {
     clean_test_files();
 
     CsvFile<int> csv("test.csv", a_num_vec, 5);
@@ -304,7 +304,7 @@ TEST(csv_search_file_intTest, csv_search_Neg) {
     clean_test_files();
 }
 
-TEST(csv_add_second_entry_intTest, csv_search_Pos) {
+TEST(csv_add_second_entry_int_test, csv_search_pos) {
     clean_test_files();
 
     a_num_vec.push_back(num_entry2);
@@ -319,7 +319,7 @@ TEST(csv_add_second_entry_intTest, csv_search_Pos) {
 }
 
 // double tests
-TEST(csv_write_read_file_doubleTest, csv_write_readPos) {
+TEST(csv_write_read_file_double_test, csv_write_read_pos) {
 
     clean_test_files();
 
@@ -341,7 +341,7 @@ TEST(csv_write_read_file_doubleTest, csv_write_readPos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_doubleTest, csv_search_Pos) {
+TEST(csv_search_file_double_test, csv_search_pos) {
     clean_test_files();
 
     CsvFile<double> csv("test.csv", a_double_vec, 5);
@@ -355,7 +355,7 @@ TEST(csv_search_file_doubleTest, csv_search_Pos) {
     clean_test_files();
 }
 
-TEST(csv_search_file_doubleTest, csv_search_Neg) {
+TEST(csv_search_file_double_test, csv_search_neg) {
     clean_test_files();
 
     CsvFile<double> csv("test.csv", a_double_vec, 5);
@@ -368,7 +368,8 @@ TEST(csv_search_file_doubleTest, csv_search_Neg) {
     clean_test_files();
 }
 
-TEST(csv_byteOrderMarkUTF8, csv_bom_Pos) {
+// byte order mark tests
+TEST(csv_byte_order_mark_UTF8, csv_bom_pos) {
     clean_test_files();
 
     string csvLine;
@@ -395,7 +396,7 @@ TEST(csv_byteOrderMarkUTF8, csv_bom_Pos) {
     clean_test_files();
 }
 
-TEST(csv_byteOrderMarkUTF16, csv_bom_Pos) {
+TEST(csv_byte_order_mark_UTF16, csv_bom_pos) {
     clean_test_files();
 
     string csvLine;
@@ -410,7 +411,8 @@ TEST(csv_byteOrderMarkUTF16, csv_bom_Pos) {
     clean_test_files();
 }
 
-TEST(csv_ConvertUTF16ToUTF8, csv_Pos) {
+// convert utf16 test
+TEST(csv_convert_UTF16_to_UTF8, csv_pos) {
     clean_test_files();
 
     string csvLine;
